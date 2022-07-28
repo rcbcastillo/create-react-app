9f2c3602e175f2f10dc90d2f03afa65821f3162f
Rutely Burgos <rcbcastillo@gmail.com>
2022-07-28 11:46:10 +0100

    Make sure changes are up to date

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Articles from './Components/Articles';
-import {Home} from './Components/Home';
-import Topics from './Components/Topics';
+import Articles from "./Components/Articles";
+import { Home } from "./Components/Home";
+import Topics from "./Components/Topics";
+import Article from "./Components/Article";
 
-
-const App = ()  => {
-    return (
-    <div>     
+const App = () => {
+  return (
+    <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>}></Route>
+          <Route path="/" element={<Home />}></Route>
           <Route path="/articles/:topic" element={<Articles />}></Route>
-          <Route path="/topics" element={<Topics />}></Route>          
+          <Route path="/topics" element={<Topics />}></Route>
+          <Route path="/article/:article_id" element={<Article />}></Route>
         </Routes>
-      </BrowserRouter>       
+      </BrowserRouter>
     </div>
-  )
-  
-}
+  );
+};
 
 export default App;
