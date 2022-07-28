6e012820104111f06e1116576ad2550b9b81e723
Rutely Burgos <rcbcastillo@gmail.com>
2022-07-28 11:37:15 +0100

    Import VoteOnArticle component and render it on Article.jsx

---
+++ src/Components/Article.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Header } from "./Header";
+import { getArticle } from "../api";
+import VoteOnArticle from "./VoteOnArticle";
+
+const Article = () => {
+  const [article, setArticle] = useState();
+  const { article_id } = useParams();
+
+  useEffect(() => {
+    getArticle(article_id).then((article) => setArticle(article));
+  }, [article_id]);
+
+  if (article) {
+    return (
+      <main>
+        <Header title={`Topic: ${article.topic}`} />
+        <section>
+          <h3>{article.title}</h3>
+          <p>{article.body}</p>
+          <p>{`Author: ${article.author}`}</p>
+          <p>{`Posted: ${article.created_at}`}</p>
+          <VoteOnArticle votes={article.votes} />
+        </section>
+      </main>
+    );
+  }
+};
+
+export default Article;
