3c5fa7fcec4b2ab7560a1fb6f62e40c02210871c
Rutely Burgos <rcbcastillo@gmail.com>
2022-07-28 11:59:58 +0100

    Make sure all changes are added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,15 @@
-import Button from 'react-bootstrap/Button';
-import Navigation  from './Navigation';
+import Navigation from "./Navigation";
 
 export const Header = ({ title }) => {
   return (
-    <div className="header"> 
+    <div className="header">
       <header>
         <h2>N C N</h2>
-        <Navigation/>
+        <Navigation />
         <div className="hero">
           <h1> {title}</h1>
-          <Button className="btn" title="See comments from users" >See comments from users</Button>
         </div>
-      </header>     
+      </header>
     </div>
   );
 };
-
