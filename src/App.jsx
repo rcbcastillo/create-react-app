559c6913ab9fcca80d72d6b9b805db62007af585
Rutely Burgos <rcbcastillo@gmail.com>
2022-07-31 11:59:14 +0100

    Change the name of Articles component to ArticlesByTopic where needed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Articles from "./Components/Articles";
+import ArticlesByTopic from "./Components/ArticlesByTopic";
 import { Home } from "./Components/Home";
 import Topics from "./Components/Topics";
 import Article from "./Components/Article";
@@ -12,7 +12,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/articles/:topic" element={<Articles />}></Route>
+          <Route path="/articles/:topic" element={<ArticlesByTopic />}></Route>
           <Route path="/topics" element={<Topics />}></Route>
           <Route path="/article/:article_id" element={<Article />}></Route>
           <Route path="/comments/:comment_id" element={<Comments />}></Route>
--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,25 +1,38 @@
-import {Link} from 'react-router-dom';
-import {FaHome, FaBookOpen,FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { FaHome, FaBookOpen, FaSearch } from "react-icons/fa";
 
 const Navigation = () => {
   return (
     <nav>
       <ul className="nav-ul">
-        <li >
-          <Link className="nav-link" to="/" aria-hidden="true">{<FaHome/>} Home</Link>
+        <li>
+          <Link className="nav-link" to="/" aria-hidden="true">
+            {<FaHome />} Home
+          </Link>
         </li>
         <li>
-          <Link className="nav-link" to="/topics" aria-hidden="true">{<FaBookOpen/>} Articles</Link>
+          <Link className="nav-link" to="/articles" aria-hidden="true">
+            {<FaBookOpen />} Articles
+          </Link>
         </li>
         <li>
-          <Link className="nav-link" to="/about">About</Link>
+          <Link className="nav-link" to="/about">
+            About
+          </Link>
         </li>
         <li>
-          <Link className="nav-link" aria-label="Search" aria-hidden="true" to="#">{<FaSearch/>}</Link>
+          <Link
+            className="nav-link"
+            aria-label="Search"
+            aria-hidden="true"
+            to="#"
+          >
+            {<FaSearch />}
+          </Link>
         </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
 export default Navigation;
