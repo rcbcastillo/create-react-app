d6b070febfcdce457576ca52f0d53676b0780c02
Rutely Burgos <rcbcastillo@gmail.com>
2022-07-31 12:54:06 +0100

    Add css style to show sortby and drop-down list

--- src/Components/DropDownList.jsx
+++ src/Components/DropDownList.jsx
@@ -1,11 +1,17 @@
 const DropListComments = () => {
   return (
-    <div>
-      <select>
-        <option value="title">Title</option>
-        <option value="created_at">Posted</option>
-        <option value="votes">Votes number</option>
-      </select>
+    <div className="sortby-container">
+      <div className="sortby-container-items">
+        <select>
+          <option value="title">Title</option>
+          <option value="created_at">Posted</option>
+          <option value="topics">Topics</option>
+          <option value="votes">Votes number</option>
+        </select>
+      </div>
+      <div>
+        <button className="sortby-container-items">Sort by</button>
+      </div>
     </div>
   );
 };
