7f184cba484ecf8a781330fcac18cd339e44f78c
Rutely Burgos <rcbcastillo@gmail.com>
2022-07-31 11:34:15 +0100

    Add DropDownList component

---
+++ src/Components/DropDownList.jsx
@@ -0,0 +1,13 @@
+const DropListComments = () => {
+  return (
+    <div>
+      <select>
+        <option value="title">Title</option>
+        <option value="created_at">Posted</option>
+        <option value="votes">Votes number</option>
+      </select>
+    </div>
+  );
+};
+
+export default DropListComments;
