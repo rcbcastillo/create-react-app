f690020eb25c25a234348bdfb39776f8fe215e2d
Rutely Burgos <rcbcastillo@gmail.com>
2022-08-01 17:26:42 +0100

    Add minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ArticlesByTopic from "./Components/ArticlesByTopic";
 import { Home } from "./Components/Home";
 import Topics from "./Components/Topics";
 import Article from "./Components/Article";
-import SearchArticles from "./Components/SearchArticles";
+import SortByArticles from "./Components/SortByArticles";
 import Comments from "./Components/Comments";
 
 const App = () => {
@@ -13,7 +13,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/articles" element={<SearchArticles />}></Route>
+          <Route path="/articles" element={<SortByArticles />}></Route>
           <Route path="/articles/:topic" element={<ArticlesByTopic />}></Route>
           <Route path="/topics" element={<Topics />}></Route>
           <Route path="/article/:article_id" element={<Article />}></Route>
